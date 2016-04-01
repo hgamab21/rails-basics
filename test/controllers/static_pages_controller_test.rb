30264605811731bd77e67e1a1920846fa8db727b
Hiram Abraham <hgamab12@gmail.com>
2016-04-01 17:26:22 -0500

    Added signup pages, stylized frames via Bootstrap

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@ror}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@ror}"
+  end
+
 end
