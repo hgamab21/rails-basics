21e73561e771161374d9181057793abb344229bd
Hiram Abraham <hgamab12@gmail.com>
2016-04-22 18:48:13 -0500

    Redo up to first login invalid test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  #get 'sessions/new'
+
+  #get 'users/new'
 
   #get 'static_pages/home'
 
@@ -10,6 +12,12 @@ Rails.application.routes.draw do
   get 'contact'   =>  'static_pages#contact'
 
   get 'signup'    =>  'users#new'
+
+  get    'login'   => 'sessions#new'
+
+  post   'login'   => 'sessions#create'
+
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
