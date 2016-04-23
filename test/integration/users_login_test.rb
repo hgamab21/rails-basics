37d132480d7e3e0747d460e493e84c48e9cd53e8
Hiram Abraham <hgamab12@gmail.com>
2016-04-23 16:24:21 -0500

    Redid up to login valid test, all things stable

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -14,4 +18,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
 end
